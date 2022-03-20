87134f170bdae3528ebf002eb3307faadc245b74
A13xMtzL <A01276785@tec.mx>
2022-03-20 00:32:04 -0600

    Detecta variables y signo igual

---
+++ lexer.h
@@ -0,0 +1,81 @@
+#pragma once
+#ifndef _LEXER
+#define _LEXER
+
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+class TipoSimbolo {
+public:
+	static const int Error = -1;
+	static const int Identificador = 0;
+	static const int Entero = 1;
+	static const int Real = 2;
+	static const int Cadena = 3;
+	static const int Tipo = 4;
+	static const int Suma = 5;
+
+	//static const int Resta = 5; // este es nuevo, para reordenar 
+
+	static const int Op_Multiplicacion = 6;
+
+	//static const int Op_Division = 5;// este es nuevo, para reordenar 
+
+	//static const int OPREL = 7;
+	//static const int OPOR = 8;
+	//static const int OPAND = 9;
+	//static const int OPNOT = 10;
+	static const int OPIGUALDAD = 11;
+	//static const int PUNTO_COMA = 12;
+	//static const int COMA = 13;
+	static const int PARENTESIS_ABRE = 14;
+	static const int PARENTESIS_CIERRA = 15;
+	//static const int LLAVE1 = 16;
+	//static const int LLAVE2 = 17;
+	static const int IGUAL = 18;
+	//static const int IF = 19;
+	//static const int WHILE = 20;
+	//static const int RETURN = 21;
+	//static const int ELSE = 22;
+	//static const int PESOS = 23;
+};
+
+class lexer {
+private:
+	string fuente;
+	int ind;
+	bool continua;
+	char c;
+	int estado;
+	int cont;
+
+	char sigCaracter();
+	void sigEstado(int estado);
+	void aceptacion(int estado);
+	bool esLetra(char c);
+	bool esDigito(char c);
+	bool esEspacio(char c);
+	void retroceso();
+
+public:
+	string simbolo;
+	int tipo;
+	string aux;
+
+	lexer(string fuente);
+	lexer();
+
+	void entrada(string fuente);
+	string tipoAcad(int tipo);
+
+	int sigSimbolo();
+	bool terminado();
+
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -13,58 +13,109 @@
 #include "lexerAritmetico.h"
 
 
+#include "lexer.h"
+
+
+
 using namespace std;
 int main() {
 
-	//---------------------------------------------------------------------------
-	
-	// Apertura del archivo y lectura de los elementos
-	//Otro comentario feo 
+//	//---------------------------------------------------------------------------
+//
+//	// Apertura del archivo y lectura de los elementos
+//	//Otro comentario feo 
+//
+//
+//	ifstream archivo("input1.txt");
+//	//archivo.open("input1.txt", ios::in);
+//
+//	string texto, temporal;
+//
+//	if (!archivo.fail()) {
+//		while (!archivo.eof()) {
+//			getline(archivo, temporal);
+//			texto += temporal + "\n";
+//
+//			archivo.close();
+//		}
+//	}
+//
+//	//------------------------**---------------------------------------------------
+//
+//	lexerAritmetico* analizador_lexico = new lexerAritmetico(); // Se hace la creacion del objeto lexer, que nos permitirá 
+//																// identificar los tokens del archivo
+//
+//	string palabras = ("b = 7"
+//
+//		"a = 32.4 * (-8.6 - b) / 6.1E-8  "
+//
+//		"d = a ^ b int x // Esto es un comentario WHILE");
+//
+//
+//
+//	analizador_lexico->entrada(palabras); //AQUI SE DEBERÍA DE ABRIR EL ARCHIVO
+//
+//
+//	cout << "\tToken\t\t|Type" << endl;
+//	cout << "-------------------------------------------------------------" << endl;
+//
+//	while (analizador_lexico->simbolo.compare("$") != 0)
+//	{
+//		analizador_lexico->sigSimbolo();
+//
+//		cout << "\t" << analizador_lexico->simbolo << "\t\t" << "|" << analizador_lexico->tipoAcad(analizador_lexico->tipo) << endl;
+//		cout << "-------------------------------------------------------------" << endl;
+//	}
+//	cin.get();
+//
+//	return 0;
+//
+//}
+//
+//
 
 
-	ifstream archivo("input1.txt");
-	//archivo.open("input1.txt", ios::in);
-
-	string texto, temporal;
-
-	if (!archivo.fail()) {
-		while (!archivo.eof()) {
-			getline(archivo, temporal);
-			texto += temporal+"\n";
-		}
-		archivo.close();
-	}
-
-	//------------------------**---------------------------------------------------
-
-	lexerAritmetico* analizador_lexico = new lexerAritmetico(); // Se hace la creacion del objeto lexer, que nos permitirá 
-																// identificar los tokens del archivo
-
-	string palabras = ("b = 7"
-
-		"a = 32.4 * (-8.6 - b) / 6.1E-8  "
-
-		"d = a ^ b int x // Esto es un comentario WHILE");
-	
-	
-	
-	analizador_lexico->entrada(palabras); //AQUI SE DEBERÍA DE ABRIR EL ARCHIVO
 
 
-	cout << "\tToken\t\t|Type" << endl;
-	cout << "-------------------------------------------------------------" << endl;
 
-	while(analizador_lexico->simbolo.compare("$")!=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//--------------------------------------------------------------------------- NUEVO ARCHIVO NOMÁS DE PRUBEA-------------------------------------------- --------------------------------- 
+
+	lexer lexer;
+
+	lexer.entrada("8 3.5 4.8 = - + ( b = 100 / 10) *");
+
+
+
+
+
+	cout << "Resultado del analisis lexico " << endl << endl;
+	cout << "Simbolo\t\tTipo" << endl;
+	while (lexer.simbolo.compare("$") != 0)
 	{
-		analizador_lexico->sigSimbolo();
+		lexer.sigSimbolo();
 
-		cout << "\t" << analizador_lexico->simbolo << "\t\t" << "|" << analizador_lexico->tipoAcad(analizador_lexico->tipo) << endl;
-		cout << "-------------------------------------------------------------" << endl;
+		cout << lexer.simbolo << "\t\t" << lexer.tipoAcad(lexer.tipo) << endl;
 	}
 	cin.get();
 
 
-	return 0;
-}
+		return 0;
 
+}
 
