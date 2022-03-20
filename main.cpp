2c751ad6c3323ddfb99993f166591c036414a1e2
A13xMtzL <A01276785@tec.mx>
2022-03-19 22:46:59 -0600

    Ultima version funcional

---
+++ main.cpp
@@ -0,0 +1,68 @@
+//	Analizador_Lexico.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+//	Codigo trabajado por: Alejandro Martínez Luna       - A01276785
+//						  Monserrat Karime Moreno Casas - A01276775
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include <sstream>
+#include <fstream>
+
+#include "lexerAritmetico.h"
+
+
+using namespace std;
+int main() {
+
+	//---------------------------------------------------------------------------
+	
+	// Apertura del archivo y lectura de los elementos
+
+	ifstream archivo("input1.txt");
+	//archivo.open("input1.txt", ios::in);
+
+	string texto, temporal;
+
+	if (!archivo.fail()) {
+		while (!archivo.eof()) {
+			getline(archivo, temporal);
+			texto += temporal+"\n";
+		}
+		archivo.close();
+	}
+
+	//------------------------**---------------------------------------------------
+
+	lexerAritmetico* analizador_lexico = new lexerAritmetico(); // Se hace la creacion del objeto lexer, que nos permitirá 
+																// identificar los tokens del archivo
+
+	string palabras = ("b = 7"
+
+		"a = 32.4 * (-8.6 - b) / 6.1E-8  "
+
+		"d = a ^ b int x // Esto es un comentario WHILE");
+	
+	
+	
+	analizador_lexico->entrada(palabras); //AQUI SE DEBERÍA DE ABRIR EL ARCHIVO
+
+
+	cout << "\tToken\t\t|Type" << endl;
+	cout << "-------------------------------------------------------------" << endl;
+
+	while(analizador_lexico->simbolo.compare("$")!=0)
+	{
+		analizador_lexico->sigSimbolo();
+
+		cout << "\t" << analizador_lexico->simbolo << "\t\t" << "|" << analizador_lexico->tipoAcad(analizador_lexico->tipo) << endl;
+		cout << "-------------------------------------------------------------" << endl;
+	}
+	cin.get();
+
+
+	return 0;
+}
+
+
